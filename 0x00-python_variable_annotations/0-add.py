43f7e50f223d2f7b58633d813cde4b9619b1eaa1
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-03-29 10:00:48 +0100

    Basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0-add.py
+Takes a float a and a float b as arguments and
+returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    'Returns the sum as a float'
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,2 @@
+This Project is part of my Backend Engineering Course, using python.
+Studying about Python Variable Annotations.
