eeded0d48e08896045b55818b38a510122c6cf95
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-02 03:49:59 +0100

    Function that multiplies coroutines at same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Multiple coroutines: returns list of all delay (float values)
